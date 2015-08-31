c03fd4be4810865c056655f4a0ba308988aac882
opalinsp <opalinsp@gmail.com>
2015-08-31 23:32:13 +0200

    ide migration

--- backup/backup.py
+++ backup/backup.py
@@ -1,15 +1,84 @@
-# import queue
-# import threading
+import queue
+import threading
 import subprocess
 import os
 
 
 class Backup:
-    """Backup class"""
+
+    """A backup class"""
+    # list of source directories
     source_directory = []
+    # list of destination directories
     destination_directory = []
+    # list of omit rules
     omit_rules = []
+
+    # determine whether we are using root or regular user account
+    if os.getlogin() == 'root':
+        prefix = 'system'
+    else:
+        prefix = 'user'
+
+    # checksum method of choice
     method = 'sha512sum'
+    # modification time attribute name
+    att_name = 'st_mtime'
+    att_sum = prefix + '.' + method
+    att_time = prefix + '.' + att_name
+
+    def __init__(self):
+        pass
+
+    def backup_start():
+        pass
+
+    def create_database_log1():
+        pass
+
+    def create_database_log2():
+        pass
+
+    def get_files():
+        pass
+
+    def log():
+        pass
+
+    def check():
+        pass
+
+    def get_file_checksum(self, file):
+        # using external checksum commands due to performance considerations
+        cmd = [self.method, file]
+import queue
+import threading
+import subprocess
+import os
+
+
+class Backup:
+
+    """A backup class"""
+    # list of source directories
+    source_directory = []
+    # list of destination directories
+    destination_directory = []
+    # list of omit rules
+    omit_rules = []
+
+    # determine whether we are using root or regular user account
+    if os.getlogin() == 'root':
+        prefix = 'system'
+    else:
+        prefix = 'user'
+
+    # checksum method of choice
+    method = 'sha512sum'
+    # modification time attribute name
+    att_name = 'st_mtime'
+    att_sum = prefix + '.' + method
+    att_time = prefix + '.' + att_name
 
     def __init__(self):
         pass
@@ -32,14 +101,45 @@ class Backup:
     def check(self):
         pass
 
-    def get_file_checksum_ext(self, root, filename):
-        cmd = [self.method, os.path.join(root, filename)]
+    def get_file_checksum(self, file):
+        # using external checksum commands due to performance considerations
+        cmd = [self.method, file]
         p = subprocess.Popen(cmd,
                              stdout=subprocess.PIPE,
                              stderr=subprocess.PIPE)
         out, err = p.communicate()
         checksum = out.decode(encoding='utf-8').split(" ")[0]
-        return checksum
+        return(checksum)
 
-    def get_file_checksum_int(self, root, filename):
-        pass
+    def get_file_mtime(self, file):
+        # using external mtime command due to issues with Python's float
+        # accuracy and lack of proper timezone formatting
+        cmd = ['stat', '--format=%y', file]
+        p = subprocess.Popen(cmd,
+                             stdout=subprocess.PIPE,
+                             stderr=subprocess.PIPE)
+        out, err = p.communicate()
+        mtime = out.decode(encoding='utf-8').split("\n")[0]
+        return(mtime)
+
+    def get_file_attributes_calc(self, file):
+        file_attributes = [None]*2
+        file_attributes[0] = self.get_file_checksum(file)
+        file_attributes[1] = self.get_file_mtime(file)
+        return(file_attributes)
+
+    def get_file_attributes_read(self, file):
+        xattr_list = os.listxattr(file)
+        file_attributes = [None]*2
+        for xattr in xattr_list:
+            if xattr == self.att_sum:
+                raw_attribute = os.getxattr(file, self.att_sum)
+                file_attributes[0] = raw_attribute.decode(encoding='utf-8')
+            elif xattr == self.att_time:
+                raw_attribute = os.getxattr(file, self.att_time)
+                file_attributes[1] = raw_attribute.decode(encoding='utf-8')
+        return(file_attributes)
+
+    def set_file_attributes(self, file, file_attributes):
+        os.setxattr(file, self.att_sum, file_attributes[0])
+        os.setxattr(file, self.att_time, file_attributes[1])
---
+++ test/test_backup.py
@@ -0,0 +1,39 @@
+import os
+import unittest
+from backup import backup
+
+
+class TestBackup(unittest.TestCase):
+
+    """ TestBackup class """
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_get_file_checksum(self):
+        root = 'tests/test_backup_files'
+        test_file_name = 'textfile.txt'
+        test_file = os.path.join(root, test_file_name)
+        checksum_calc = backup.Backup().get_file_checksum(test_file)
+        test_file_sum_name = 'textfile.txt.sum'
+        test_file_sum = os.path.join(root, test_file_sum_name)
+        f = open(test_file_sum)
+        checksum_read = f.read().split(" ")[0]
+        f.close()
+        self.assertEqual(checksum_calc, checksum_read)
+
+    def test_get_file_attributes(self):
+        root = 'tests/test_backup_files'
+        test_file_name = 'flacfile.flac'
+        test_file = os.path.join(root, test_file_name)
+        file_attributes_calc = [None]*2
+        file_attributes_calc[0] = backup.Backup().get_file_checksum(test_file)
+        file_attributes_calc[1] = backup.Backup().get_file_mtime(test_file)
+        file_attributes_read = backup.Backup().get_file_attributes(test_file)
+        self.assertEqual(file_attributes_calc, file_attributes_read)
+
+if __name__ == '__main__':
+    unittest.main()
